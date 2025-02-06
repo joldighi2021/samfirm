39689c483a1d7a6848a68d1f274647b5957095e5
joldighi2021 <joldighi2021@gmail.com>
2025-02-06 17:28:31 +0600

    Update install_requirements.sh

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -1,9 +1,9 @@
 echo "Updating system packages"
-sudo apt update -y
+apt update -y
 
 echo "Installing python3"
 # Python3 is for samfirm
-sudo apt update python3 -y
+apt update python3 -y
 
 echo "Installing samloader"
 pip3 install git+https://github.com/martinetd/samloader.git
