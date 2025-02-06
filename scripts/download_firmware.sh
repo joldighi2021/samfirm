d8e1fd9d1398504b4f57f46f02327424e2878fad
joldighi2021 <joldighi2021@gmail.com>
2025-02-06 18:29:02 +0600

    Update download_firmware.sh

--- scripts/download_firmware.sh
+++ scripts/download_firmware.sh
@@ -14,8 +14,7 @@ if [ ${#MODEL} -ne 8 ] || [ ${#CSC} -ne 3 ]; then
 fi
 
 # Read IMEIs from imei.txt for the given MODEL
-ls
-IMEI_LIST=$(grep "^${MODEL}=" $(pwd)/imei.txt | cut -d '=' -f2 | tr -d '"' | tr ',' ' ')
+IMEI_LIST=$(grep "^${MODEL}=" $(pwd)/scripts/imei.txt | cut -d '=' -f2 | tr -d '"' | tr ',' ' ')
 
 if [ -z "$IMEI_LIST" ]; then
     echo "No IMEI found for model $MODEL in imei.txt"
