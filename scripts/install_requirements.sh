ff564531e6382c04846047fc5b97e5d3ae0758e8
joldighi2021 <joldighi2021@gmail.com>
2025-02-06 20:41:16 +0600

    Update install_requirements.sh

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -7,3 +7,6 @@ apt install -y python3
 
 echo "Installing samloader"
 pip3 install git+https://github.com/martinetd/samloader.git
+
+echo "Installing packages for extract firmware"
+sudo apt install -y python3 p7zip-full lz4 android-sdk-libsparse-utils && pip3 install liblp
