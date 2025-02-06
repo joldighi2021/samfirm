c059cf595bce84b433cd310bca9c002eb98b74e7
joldighi2021 <joldighi2021@gmail.com>
2025-02-06 20:54:53 +0600

    Update extract_firmware.sh

--- scripts/extract_firmware.sh
+++ scripts/extract_firmware.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
 echo -e "Extracting Firmware."
-cd pwd/Downloads
+cd "$(pwd)/Downloads"
 sudo 7z x firmware.zip && sudo rm -rf firmware.zip && sudo rm -rf *.txt && for file in *.md5; do sudo mv -- "$file" "${file%.md5}"; done
 
 echo -e "Extracting tar files."
-for file in *.tar; do sudo tar -xvf "$file"; done
-    sudo find . -type f ! -name 'super.img.lz4' ! -name 'optics.img.lz4' ! -name 'prism.img.lz4' ! -name 'boot.img.lz4' -delete
-    sudo rm -rf *.tar
-    sudo rm -rf meta-data
+for file in *.tar; do
+    sudo tar -xvf "$file"
+done
+sudo find . -type f ! -name 'super.img.lz4' ! -name 'optics.img.lz4' ! -name 'prism.img.lz4' ! -name 'boot.img.lz4' -delete
+sudo rm -rf *.tar
+sudo rm -rf meta-data
 
 echo -e "Extracting lz4 files."
-for file in *.lz4; do sudo lz4 -d "$file" "${file%.lz4}"; done
-    sudo rm -rf *.lz4
+for file in *.lz4; do
+    sudo lz4 -d "$file" "${file%.lz4}"
+done
+sudo rm -rf *.lz4
 
 echo -e "Converting all images to raw img."
 for file in *.img; do
@@ -23,8 +27,10 @@ for file in *.img; do
     fi
 done
 
-sudo find \. -type f -name '*\.img' \! -name 'boot\.img' -delete
-for file in *.raw; do sudo mv -- "$file" "${file%.raw}"; done
+sudo find . -type f -name '*.img' ! -name 'boot.img' -delete
+for file in *.raw; do
+    sudo mv -- "$file" "${file%.raw}"
+done
 
 echo -e "Check and extract all partitions from super.img."
 if [ -f "super.img" ]; then
@@ -35,5 +41,7 @@ else
     echo "super.img not found. Skipping..."
 fi
 
-echo -e "Compressing all images to xz format.
-for i in *.img; do 7z a -mx9 "${i%.*}.img.xz" "$i" && rm "$i"; done
+echo -e "Compressing all images to xz format."
+for i in *.img; do
+    7z a -mx9 "${i%.*}.img.xz" "$i" && rm "$i"
+done
